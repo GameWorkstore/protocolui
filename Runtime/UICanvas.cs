246d7210932dcc1eb64981d8765079b35d3e0bbe
Douglas Rachevsky <douglas@protagames.com>
2022-04-19 16:19:58 -0300

    optional isActive as true on SetState
    version 1.1.1

---
+++ Runtime/UICanvas.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using GameWorkstore.Patterns;
+
+namespace GameWorkstore.ProtocolUI
+{
+    public class UICanvas : MonoBehaviour
+    {
+        public StatePreview[] LayeredStates;
+        public UIStateScriptable[] ActiveStates;
+        public UIStateScriptable[] EditorActiveStates;
+        private readonly HighSpeedArray<UIPanel> _panels = new HighSpeedArray<UIPanel>(128);
+
+        private void Awake()
+        {
+            for(int i = 0; i < SceneManager.sceneCount; i++)
+            {
+                foreach (var panels in SceneManager.GetSceneAt(i).GetRootGameObjects().Select(t => t.GetComponentsInChildren<UIPanel>(true)))
+                {
+                    foreach(var panel in panels)
+                    {
+                        _panels.Add(panel);
+                        panel.Register();
+                    }
+                }
+            }
+
+            var _stateService = ServiceProvider.GetService<UIStateService>();
+            for (int i = 0; i < LayeredStates.Length; i++)
+            {
+                _stateService.RegisterState(LayeredStates[i].State, LayeredStates[i].Layer);
+            }
+#if UNITY_EDITOR
+            for (int i = 0; i < EditorActiveStates.Length; i++)
+            {
+                _stateService.SetState(EditorActiveStates[i], true);
+            }
+#else
+            for(int i=0;i < ActiveStates.Length; i++)
+            {
+                _stateService.SetState(ActiveStates[i], true);
+            }
+#endif
+        }
+
+        private void OnDestroy()
+        {
+            _panels.ForEach(t => t.Unregister());
+        }
+    }
+}
---
+++ Runtime/UIPanel.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using GameWorkstore.Patterns;
+
+namespace GameWorkstore.ProtocolUI
+{
+    public class UIPanel : MonoBehaviour
+    {
+        public UIStateScriptable[] ActiveStates;
+        public Selectable FirstSelected;
+        public bool SetVisibilityOnShow = true;
+        public bool SetVisibilityOnHide = true;
+
+        public static int FrameUpdate = 10;
+        private UIStateService _stateService;
+        private bool _initialized = false;
+        private int _frameCount = 0;
+
+        public virtual void Register()
+        {
+            _stateService = ServiceProvider.GetService<UIStateService>();
+            for (int i = 0; i < ActiveStates.Length; i++)
+            {
+                _stateService.RegisterState(ActiveStates[i]);
+            }
+            ServiceProvider.GetService<EventService>().Update.Register(UpdatePanel);
+        }
+
+        public virtual void Unregister()
+        {
+            ServiceProvider.GetService<EventService>().Update.Unregister(UpdatePanel);
+        }
+
+        public void UpdatePanel()
+        {
+            if (_frameCount++ % FrameUpdate > 0) return;
+
+            bool isVisible = _stateService.IsAnyStateActive(ref ActiveStates);
+
+            if (gameObject.activeSelf != isVisible || !_initialized && isVisible)
+            {
+                _initialized = true;
+                if (isVisible)
+                {
+                    OnShow();
+                    if (SetVisibilityOnShow)
+                    {
+                        CompleteShow();
+                    }
+                }
+                else
+                {
+                    if (SetVisibilityOnHide)
+                    {
+                        CompleteHide();
+                    }
+                    OnHide();
+                }
+
+                if (isVisible && FirstSelected != null)
+                {
+                    EventSystem.current.SetSelectedGameObject(null);
+                    EventSystem.current.SetSelectedGameObject(FirstSelected.gameObject);
+                }
+            }
+        }
+
+        /// <summary>
+        /// The framework requests to the panel to become visible.
+        /// </summary>
+        public virtual void OnShow() { }
+
+        /// <summary>
+        /// The framework requests to the panel to become invisible.
+        /// </summary>
+        public virtual void OnHide() { }
+
+        protected void CompleteShow()
+        {
+            gameObject.SetActive(true);
+        }
+
+        protected void CompleteHide()
+        {
+            gameObject.SetActive(false);
+        }
+
+
+        public void Select(GameObject gameObject)
+        {
+            if (gameObject == null) return;
+
+            EventSystem.current.SetSelectedGameObject(null);
+            EventSystem.current.SetSelectedGameObject(gameObject);
+        }
+
+        public void Select(Selectable selectable)
+        {
+            if (selectable == null) return;
+
+            EventSystem.current.SetSelectedGameObject(null);
+            EventSystem.current.SetSelectedGameObject(selectable.gameObject);
+        }
+
+        public void ClearSelection()
+        {
+            EventSystem.current.SetSelectedGameObject(null);
+            EventSystem.current.SetSelectedGameObject(null);
+        }
+    }
+}
