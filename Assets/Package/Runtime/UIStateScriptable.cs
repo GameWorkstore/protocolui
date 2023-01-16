2e12b903de8271ad102510204e042ce62d545f72
HanzaRu <woodimbr@gmail.com>
2023-01-16 01:30:57 +0100

    Version 1.2.4
    Fixed state hash confusion on Unity Editor due to cache. Performance will be slower on unity editor, but will guarantee that hash is correct during game edition.

--- Assets/Package/Runtime/UIStateScriptable.cs
+++ Assets/Package/Runtime/UIStateScriptable.cs
@@ -2,20 +2,26 @@ using UnityEngine;
 
 namespace GameWorkstore.ProtocolUI
 {
-    [CreateAssetMenu(fileName=nameof(UIStateScriptable),menuName="ProtocolUI/"+nameof(UIStateScriptable))]
+    [CreateAssetMenu(fileName = nameof(UIStateScriptable), menuName = "ProtocolUI/" + nameof(UIStateScriptable))]
     public class UIStateScriptable : ScriptableObject
     {
+#if !UNITY_EDITOR
         private bool _IsInitialized = false;
         private int _cache = -1;
+#endif
 
         public int Hash
         {
             get
             {
+#if UNITY_EDITOR
+                return Animator.StringToHash(name);
+#else
                 if (_IsInitialized) return _cache;
                 _cache = Animator.StringToHash(name);
                 _IsInitialized = true;
                 return _cache;
+#endif
             }
         }
     }
